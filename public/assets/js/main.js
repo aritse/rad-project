077250ef727b610ca5cb164311f16f692ea755ee
Phillip Dunham <phillipwwd@gmail.com>
2020-01-22 13:32:51 -0800

    Changing to a tags, both reference /register

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,25 @@
+import { parseWithoutProcessing } from "handlebars";
+
 console.log("this is main.js");
+
+
+// $("customerregister").on(click,handlercuterom regists) 
+
+
+
+// function handlecustomer regist (event) {
+// event.defaultPrevented();
+// const fistname = $("#fistnameinput").val();
+// lastname
+// phone
+
+// cosnt customer = {
+//     firstName:firstName,
+//     lastname:lastname
+// }
+
+// $R.ajax(
+//     "/regester",
+//     data: customer
+// )
+// }
