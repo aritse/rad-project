6a1c634d5336b30f78fdd7152b607d7b28b1ba91
chrisp-dev <chris.paradeise@gmail.com>
2020-01-23 12:54:46 -0800

    Added express-session # PLEASE ADD YOUR OWN .ENV FILE SESSION_SECRET=""

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const hbs = require('express-handlebars');
 var hbs2 = require('hbs');
 require('handlebars-form-helpers').register(hbs2.handlebars);
-const express = require("express");
-const hbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 3300;
 const db = require("./models");
+const session = require('express-session');
+require('dotenv').config();
 
 app.engine("handlebars", hbs({ defaultDisplay: "main" }));
 app.set("view engine", "handlebars");
@@ -15,6 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+
+// init session
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true, saveUninitialized: true,
+  cookie: { maxAge: 7200000 }
+}));
 
 require("./routes/html-routes")(app);
 require("./routes/customer-routes")(app);
